af66e7aecbece96cb86ea4ef81777534ecca90cf
Salmaan Saeed <salsaeed999@gmail.com>
2023-03-25 22:41:15 -0400

    created hello world gui window using qt5

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # MessageCliApp
+
+
+
+
+sudo apt-get install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
+#include <QApplication>
+#include <QPushButton>
 
 #include "message/message.h"
 
-int main() {
+int main(int argc, char **argv)
+{
+    /*
     std::string input;
     std::cin >> input;
     Message message = Message(input);
     std::cout << message.get_user_input() << std::endl;
-}
+    */
 
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello world !");
+    button.show();
+
+    return app.exec();
+}
