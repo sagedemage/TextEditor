99cbc213943c20b3400e985308a45b4957bf4a0b
Salmaan Saeed <salsaeed999@gmail.com>
2023-04-05 00:57:41 -0400

    made window bigger

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
     
     MainWindow mainWindow;
-    mainWindow.resize(480, 360);
+    mainWindow.resize(840, 630);
     mainWindow.show();
 
     return app.exec();
